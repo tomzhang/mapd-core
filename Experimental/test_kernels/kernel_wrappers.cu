96e718bffc2b89607c689da86baf8b33e124ce59
Todd Mostak <todd@map-d.com>
2014-12-03 00:05:18 -0800

    Working on string search kernel - not getting expected output

--- Experimental/test_kernels/kernel_wrappers.cu
+++ Experimental/test_kernels/kernel_wrappers.cu
@@ -1,7 +1,12 @@
 #include "string_kernels.cu"
 
 void cudaVLikeCWarpWrapper(const char *data, const unsigned int *startIndex, const unsigned int *endIndex, const unsigned int numElements, const unsigned int searchSlotsLen, unsigned int * matchCount,const size_t numBlocks, const size_t numThreads) {
-    cudaVLikeCWarp <<<numBlocks,numThreads>>> (data,startIndex,endIndex,numElements,searchSlotsLen,matchCount);
+
+   unsigned int hostSearchData = 1701336074;
+   //unsigned int hostSearchData = 1701603616;
+   cudaMemcpyToSymbol(searchData, &hostSearchData, sizeof(unsigned int)); 
+
+    cudaVLikeCWarp <<<numBlocks,numThreads>>> ((unsigned int *) data,startIndex,endIndex,numElements,searchSlotsLen,matchCount, hostSearchData);
 }
 
 
--- Experimental/test_kernels/string_kernels.cu
+++ Experimental/test_kernels/string_kernels.cu
@@ -1,6 +1,8 @@
 #define WARPSIZE 32
 
-__global__ void cudaVLikeCWarp(const char *data, const unsigned int *startIndex, const unsigned int *endIndex, const unsigned int numElements, const unsigned int searchSlotsLen, unsigned int * matchCount) { 
+__constant__ unsigned int searchData[8];
+
+__global__ void cudaVLikeCWarp(const unsigned int *data, const unsigned int *startIndex, const unsigned int *endIndex, const unsigned int numElements, const unsigned int searchSlotsLen, unsigned int * matchCount, const unsigned int searchInt) { 
     __shared__ unsigned int sharedMatchCount;
     if (threadIdx.x == 0) {
         sharedMatchCount = 0;
@@ -10,27 +12,42 @@ __global__ void cudaVLikeCWarp(const char *data, const unsigned int *startIndex,
     int stride = blockDim.x * gridDim.x;
     int warpId = threadIdx.x / WARPSIZE;
     int warpThreadId = threadIdx.x % WARPSIZE;
-    int warpStride = 4096 * WARPSIZE / blockDim.x;
-    int warpOutOffset = warpId * warpStride;
+    //int warpStride = 4096 * WARPSIZE / blockDim.x;
+    //int warpOutOffset = warpId * warpStride;
     unsigned int localMatchCount = 0;
-    while (i < numElements) {
+    while (i < numElements - 10000) {
         unsigned int indexStart = startIndex[i];
         unsigned int indexEnd = endIndex[i] - searchSlotsLen;
-        unsigned int docSlotStart = indexStart / 4;
-        unsigned int docSlotEnd = indexEnd  / 4 + (indexEnd % 4 != 0);
-        localMatchCount++;
+        int slotStart = indexStart / 4; 
+        int slotEnd = indexEnd / 4; 
+        int slotMod = indexEnd % 4;
+        //int docSlotStart = indexStart / 4;
+        //int docSlotEnd = indexEnd  / 4 + (indexEnd % 4 != 0);
         for (int curWarpDoc = 0; curWarpDoc != 32; ++curWarpDoc) {
-            /*
-            unsigned int docStart = __shfl(int(readStart),curWarpDoc,WARPSIZE);
-            unsigned int docEnd = __shfl(int(readEnd),curWarpDoc,WARPSIZE);
-            unsigned int lastIndex = docEnd - searchSlotsLen; 
-            */
+            int docStart = __shfl(slotStart,curWarpDoc,WARPSIZE);
+            int docEnd = __shfl(slotEnd,curWarpDoc,WARPSIZE);
+            int docMod = __shfl(slotMod,curWarpDoc,WARPSIZE);
             bool found = false;
+            unsigned int offset = docStart + warpThreadId;
+            while (offset < docEnd) {
+                if (data[offset] == searchInt)
+                    found = true;
+                offset += WARPSIZE;
+            }
 
+            int warpFound = __any(found);
+            if (warpThreadId == 0 && warpFound) {
+                localMatchCount++;
+            }
+            //int numWarpCycles = (docEnd-docStart+WARPSIZE)/WARPSIZE; // because end is inclusive here
+            //numCycles =    
+            //for (unsigned int cIndex = docStart + warpThreadId; cIndex < lastIndex; cIndex += WARPSIZE) {
         }
+
         i += stride;
     }
-    atomicAdd(matchCount, localMatchCount);
+    if (warpThreadId == 0) 
+        atomicAdd(matchCount, localMatchCount);
 }
 
 
