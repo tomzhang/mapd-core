9554f3abf7386b5fd0383d42f27018d39dda9836
Christopher Root <chris@mapd.com>
2016-03-28 14:30:23 -0700

    Adding first draft of a GLShaderBlockLayout class for buffer-backed shader blocks

---
+++ Rendering/Renderer/GL/Resources/GLShaderBlockLayout.h
@@ -0,0 +1,93 @@
+#ifndef RENDERING_GL_RESOURCES_GLSHADERBLOCKLAYOUT_H_
+#define RENDERING_GL_RESOURCES_GLSHADERBLOCKLAYOUT_H_
+
+#include "GLBufferLayout.h"
+#include "GLShader.h"
+
+namespace Rendering {
+namespace GL {
+namespace Resources {
+
+class GLShaderBlockLayout : public GLBaseBufferLayout {
+ public:
+  // see: https://www.opengl.org/wiki/Interface_Block_(GLSL)#Memory_layout
+  // for a description of the different layout types
+  enum class LayoutType { PACKED = 0, SHARED, STD140, STD430 };
+
+  // all shader block layouts must be interleaved, but there are rules for
+  // the stride/offset for each layout type, hence the reasoning for
+  // a specific shader block layout.
+  GLShaderBlockLayout(const GLShaderShPtr& shaderPtr, size_t blockByteSize, LayoutType layoutType = LayoutType::SHARED)
+      : GLBaseBufferLayout(GLBufferLayoutType::INTERLEAVED), _shaderPtr(shaderPtr), _layoutType(layoutType) {
+    _vertexByteSize = blockByteSize;
+  }
+
+  ~GLShaderBlockLayout() {}
+
+  LayoutType getLayoutType() const { return _layoutType; }
+
+  size_t getNumBytesInBlock() const { return _vertexByteSize; }
+
+ private:
+  template <typename T, int numComponents = 1>
+  void addAttribute(const std::string& attrName, GLuint attrIdx) {
+    RUNTIME_EX_ASSERT(!hasAttribute(attrName) && attrName.length(),
+                      "GLShaderBlockLayout::addAttribute(): attribute " + attrName + " already exists in the layout.");
+
+    CHECK(attrIdx == _attrMap.size()) << "Trying to add an attribute " << attrName
+                                      << " to GLShaderBlockLayout, but it's being added out of order. It's index is "
+                                      << attrIdx << " but the attr with index " << _attrMap.size()
+                                      << " is next in line to be added.";
+
+    GLint offset = 0;
+
+    switch (_layoutType) {
+      case LayoutType::PACKED:
+      case LayoutType::SHARED: {
+        GLShaderShPtr shader = _shaderPtr.lock();
+        CHECK(shader != nullptr);
+        MAPD_CHECK_GL_ERROR(glGetActiveUniformsiv(shader->getId(), 1, &attrIdx, GL_UNIFORM_OFFSET, &offset));
+        CHECK(offset >= 0) << "Attribute " << attrName
+                           << " is not a part of a shader storage block. Cannot retrieve its offset";
+        break;
+      }
+
+      case LayoutType::STD140:
+        THROW_RUNTIME_EX("Layout type std140 has yet to be implemented.")
+        break;
+      case LayoutType::STD430:
+        THROW_RUNTIME_EX("Layout type std430 has yet to be implemented.")
+        break;
+      default:
+        THROW_RUNTIME_EX("Unsupported shader storage block layout type: " +
+                         std::to_string(static_cast<int>(_layoutType)));
+        break;
+    }
+
+    GLBufferAttrType type = detail::getBufferAttrType(T(0), numComponents);
+    int enumVal = static_cast<int>(type);
+    _attrMap.push_back(
+        BufferAttrInfoPtr(new GLBufferAttrInfo(attrName, type, detail::attrTypeInfo[enumVal].get(), -1, offset)));
+  }
+
+  void bindToShader(GLShader* activeShader,
+                    int numActiveBufferItems,
+                    const std::string& attr = "",
+                    const std::string& shaderAttr = "") {
+    THROW_RUNTIME_EX(
+        "A GLShaderBlockLayout cannot be bound to a shader. It is instead defined by a shader via a shader storage "
+        "block.")
+  }
+
+  GLShaderWkPtr _shaderPtr;
+  LayoutType _layoutType;
+
+  friend class ::Rendering::GL::Resources::GLShader;
+  friend struct ::Rendering::GL::Resources::detail::UniformBlockAttrInfo;
+};
+
+}  // namespace Resources
+}  // namespace GL
+}  // namespace Rendering
+
+#endif  // RENDERING_GL_RESOURCES_GLSHADERBLOCKLAYOUT_H_
