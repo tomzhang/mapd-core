tomzhang/mapd-core

commit 2eab04db2033a0d6e89ade893bc5d923ffe16460

Immediately map opengl buffers to cuda for better gpu memory management

QueryRenderer/QueryRenderManager.cpp
@@ -59,7 +59,11 @@ QueryRenderManager::ActiveRendererGuard::~ActiveRendererGuard() {
   }
 }
 
-QueryRenderManager::QueryRenderManager(int numGpus, int startGpu, size_t queryResultBufferSize, size_t renderCacheLimit)
+QueryRenderManager::QueryRenderManager(CudaMgr_Namespace::CudaMgr* cudaMgr,
+                                       int numGpus,
+                                       int startGpu,
+                                       size_t queryResultBufferSize,
+                                       size_t renderCacheLimit)
     : _rendererMap(),
       _activeItr(_rendererMap.end()),
       _perGpuData(new PerGpuDataMap()),
@@ -68,10 +72,11 @@ QueryRenderManager::QueryRenderManager(int numGpus, int startGpu, size_t queryRe
   // NOTE: this constructor needs to be used on the main thread as that is a requirement
   // for a window manager instance.
   ::Rendering::WindowManager windowMgr;
-  _initialize(windowMgr, numGpus, startGpu, queryResultBufferSize);
+  _initialize(windowMgr, cudaMgr, numGpus, startGpu, queryResultBufferSize);
 }
 
 QueryRenderManager::QueryRenderManager(Rendering::WindowManager& windowMgr,
+                                       CudaMgr_Namespace::CudaMgr* cudaMgr,
                                        int numGpus,
                                        int startGpu,
                                        size_t queryResultBufferSize,
@@ -81,13 +86,14 @@ QueryRenderManager::QueryRenderManager(Rendering::WindowManager& windowMgr,
       _perGpuData(),
       _compositorPtr(nullptr),
       _renderCacheLimit(renderCacheLimit) {
-  _initialize(windowMgr, numGpus, startGpu, queryResultBufferSize);
+  _initialize(windowMgr, cudaMgr, numGpus, startGpu, queryResultBufferSize);
 }
 
 QueryRenderManager::~QueryRenderManager() {
 }
 
 void QueryRenderManager::_initialize(Rendering::WindowManager& windowMgr,
+                                     CudaMgr_Namespace::CudaMgr* cudaMgr,
                                      int numGpus,
                                      int startGpu,
                                      size_t queryResultBufferSize) {
@@ -131,6 +137,11 @@ void QueryRenderManager::_initialize(Rendering::WindowManager& windowMgr,
   ActiveRendererGuard renderGuard;
 
   for (size_t i = startDevice; i < endDevice; ++i) {
+    if (cudaMgr) {
+      // need to set a cuda context before creating gl/cuda interop buffers
+      cudaMgr->setContext(i);
+    }
+
     windowSettings.setStrSetting(StrSetting::NAME, windowName + std::to_string(i));
     windowSettings.setIntSetting(IntSetting::GPU_ID, i);
 

MapDServer.cpp
@@ -106,17 +106,6 @@ class MapDHandler : virtual public MapDIf {
       executor_device_type_ = ExecutorDeviceType::GPU;
       LOG(INFO) << "Started in GPU Mode" << std::endl;
       cpu_mode_only_ = false;
-
-#ifdef HAVE_RENDERING
-      if (enable_rendering_) {
-        try {
-          render_manager_.reset(new ::QueryRenderer::QueryRenderManager(num_gpus, start_gpu, render_mem_bytes));
-        } catch (const std::exception& e) {
-          enable_rendering_ = false;
-          LOG(ERROR) << "Backend rendering disabled: " << e.what();
-        }
-      }
-#endif
     } else if (executor_device == "hybrid") {
       executor_device_type_ = ExecutorDeviceType::Hybrid;
       LOG(INFO) << "Started in Hybrid Mode" << std::endl;
@@ -129,6 +118,22 @@ class MapDHandler : virtual public MapDIf {
     const auto data_path = boost::filesystem::path(base_data_path_) / "mapd_data";
     data_mgr_.reset(
         new Data_Namespace::DataMgr(data_path.string(), cpu_buffer_mem_bytes, !cpu_mode_only_, num_gpus, start_gpu));
+
+#ifdef HAVE_RENDERING
+    // TODO(croot): remove the cpu_mode_only_ rendering restriction
+    // when cpu-mode with hardware accelerated rendering
+    // or cpu-mode with software rendering is supported.
+    if (enable_rendering_ && !cpu_mode_only_) {
+      try {
+        render_manager_.reset(
+            new ::QueryRenderer::QueryRenderManager(data_mgr_->cudaMgr_, num_gpus, start_gpu, render_mem_bytes));
+      } catch (const std::exception& e) {
+        enable_rendering_ = false;
+        LOG(ERROR) << "Backend rendering disabled: " << e.what();
+      }
+    }
+#endif
+
     sys_cat_.reset(new Catalog_Namespace::SysCatalog(base_data_path_, data_mgr_, ldapMetadata));
     import_path_ = boost::filesystem::path(base_data_path_) / "mapd_import";
   }

QueryRenderer/CMakeLists.txt
@@ -6,7 +6,7 @@ find_package(CUDA REQUIRED)
 
 set(QUERY_RENDERER_SOURCES QueryRenderManager.cpp QueryFramebuffer.cpp QueryRenderer.cpp QueryRendererObjects.cpp QueryDataTable.cpp RapidJSONUtils.cpp QueryResultVertexBuffer.cpp QueryDataLayout.cpp QueryRenderCompositor.cpp QueryIdMapPboPool.cpp QueryIdMapPixelBuffer.cpp PngData.cpp Utilities.cpp Types.cpp)
 
-set(QUERY_RENDERER_LIBS ${Boost_LIBRARIES} ${OPENGL_LIBRARIES} ${PNG_LIBRARIES} ${CUDA_LIBRARIES} ${Glog_LIBRARIES})
+set(QUERY_RENDERER_LIBS ${Boost_LIBRARIES} ${OPENGL_LIBRARIES} ${PNG_LIBRARIES} ${CUDA_LIBRARIES} ${Glog_LIBRARIES} CudaMgr)
 
 set(QUERY_RENDERER_INCLUDE_DIRS ${Boost_INCLUDE_DIR} ${GL_INCLUDE_DIR} ${PNG_INCLUDE_DIR} ${CUDA_INCLUDE_DIR} ${Glog_INCLUDE_DIR} ThirdParty/rapidjson)
 

QueryRenderer/QueryRenderManager.h
@@ -26,12 +26,9 @@
 #include <boost/multi_index/member.hpp>
 #include <boost/multi_index/tag.hpp>
 
-#ifdef HAVE_CUDA
 #include <CudaMgr/CudaMgr.h>
-#endif  // HAVE_CUDA
 
 class Executor;
-class CudaMgr;
 
 namespace QueryRenderer {
 
@@ -152,11 +149,13 @@ class QueryRenderManager {
 
   typedef PerGpuDataMap::index<inorder>::type PerGpuDataMap_in_order;
 
-  explicit QueryRenderManager(int numGpus = -1,
+  explicit QueryRenderManager(CudaMgr_Namespace::CudaMgr* cudaMgr,
+                              int numGpus = -1,
                               int startGpu = 0,
                               size_t queryResultBufferSize = 500000,
                               size_t renderCacheLimit = 500);
   explicit QueryRenderManager(Rendering::WindowManager& windowMgr,
+                              CudaMgr_Namespace::CudaMgr* cudaMgr,
                               int numGpus = -1,  // < 0 means use all available GPUs
                               int startGpu = 0,
                               size_t queryResultBufferSize = 500000,
@@ -260,7 +259,11 @@ class QueryRenderManager {
   std::shared_ptr<PerGpuDataMap> _perGpuData;
   std::shared_ptr<QueryRenderCompositor> _compositorPtr;
 
-  void _initialize(Rendering::WindowManager& windowMgr, int numGpus, int startGpu, size_t queryResultBufferSize);
+  void _initialize(Rendering::WindowManager& windowMgr,
+                   CudaMgr_Namespace::CudaMgr* cudaMgr,
+                   int numGpus,
+                   int startGpu,
+                   size_t queryResultBufferSize);
   void _resetQueryResultBuffers() noexcept;
 
   void _setActiveUserWidget(int userId, int widgetId) const;

QueryRenderer/QueryResultVertexBuffer.cpp
@@ -87,6 +87,9 @@ QueryResultVertexBuffer::QueryResultVertexBuffer(Rendering::GL::GLRenderer* rend
       _gpuId(-1),
       _cudaResourceMap() {
   _gpuId = renderer->getGpuId();
+#ifdef HAVE_CUDA
+  _initCudaGraphicsResource();
+#endif
 }
 
 QueryResultVertexBuffer::~QueryResultVertexBuffer() {
@@ -187,7 +190,7 @@ CudaHandle QueryResultVertexBuffer::getCudaHandlePreQuery() {
       "QueryResultVertexBuffer " + std::to_string(_gpuId) + " is already in use. Cannot access cuda handle.");
 
   // now map the buffer for cuda
-  CUgraphicsResource cudaRsrc = _getCudaGraphicsResource(true);
+  CUgraphicsResource cudaRsrc = _getCudaGraphicsResource();
   _mapCudaGraphicsResource(cudaRsrc);
 
   size_t num_bytes = 0;
@@ -206,8 +209,39 @@ CudaHandle QueryResultVertexBuffer::getCudaHandlePreQuery() {
   return CudaHandle(reinterpret_cast<void*>(devPtr), num_bytes);
 }
 
-CUgraphicsResource QueryResultVertexBuffer::_getCudaGraphicsResource(bool registerResource) {
-  // CUcontext* currCudaCtx=nullptr;
+void QueryResultVertexBuffer::_initCudaGraphicsResource() {
+  CUcontext currCudaCtx;
+  CUdevice ctxDevice;
+
+  checkCudaErrors(cuCtxGetCurrent(&currCudaCtx));
+  checkCudaErrors(cuCtxGetDevice(&ctxDevice));
+
+  RUNTIME_EX_ASSERT(ctxDevice == _gpuId,
+                    "QueryResultVertexBuffer " + std::to_string(_gpuId) +
+                        ": Invalid cuda context for QueryResultVertexBuffer. Device " + std::to_string(ctxDevice) +
+                        " for cuda context does not match the QueryResultVertexBuffer device " +
+                        std::to_string(_gpuId));
+
+  CHECK(currCudaCtx != nullptr);
+  CHECK(_vbo);
+  GLuint rsrcId = _vbo->getId();
+  CHECK(rsrcId);
+
+  const auto itr = _cudaResourceMap.find(currCudaCtx);
+  if (itr == _cudaResourceMap.end()) {
+    CUgraphicsResource rsrc;
+    CUresult result = cuGraphicsGLRegisterBuffer(&rsrc, rsrcId, CU_GRAPHICS_REGISTER_FLAGS_WRITE_DISCARD);
+    RUNTIME_EX_ASSERT(
+        result != CUDA_ERROR_UNKNOWN,
+        "Cuda error code=" + std::to_string(result) +
+            ", CUDA_ERROR_UNKNOWN, possibly not enough gpu memory available for the requested buffer size of " +
+            std::to_string(_vbo->numBytes()) + " bytes.");
+    checkCudaErrors(result);
+    _cudaResourceMap.insert(std::make_pair(currCudaCtx, rsrc));
+  }
+}
+
+CUgraphicsResource QueryResultVertexBuffer::_getCudaGraphicsResource() {
   CUcontext currCudaCtx;
   CUdevice ctxDevice;
 
@@ -227,15 +261,7 @@ CUgraphicsResource QueryResultVertexBuffer::_getCudaGraphicsResource(bool regist
   CHECK(rsrcId);
 
   const auto itr = _cudaResourceMap.find(currCudaCtx);
-  if (itr == _cudaResourceMap.end()) {
-    CHECK(registerResource);
-
-    CUgraphicsResource rsrc;
-    checkCudaErrors(cuGraphicsGLRegisterBuffer(&rsrc, rsrcId, CU_GRAPHICS_REGISTER_FLAGS_WRITE_DISCARD));
-    _cudaResourceMap.insert(std::make_pair(currCudaCtx, rsrc));
-    return rsrc;
-  }
-
+  CHECK(itr != _cudaResourceMap.end());
   return itr->second;
 }
 

QueryRenderer/QueryResultVertexBuffer.h
@@ -4,30 +4,13 @@
 #include <Rendering/Renderer/GL/Resources/GLVertexBuffer.h>
 #include <Rendering/Renderer/GL/Types.h>
 
-// #include "MapDGL.h"
-// #include "QueryRendererError.h"
-// #include "BufferLayout.h"
-// #include "Shader.h"
-// #include "VertexBuffer.h"
 #include <GL/glew.h>
-// #include <glog/logging.h>
 
 #ifdef HAVE_CUDA
-// // CUDA libs
-// // #include <cuda_runtime.h>
-// // #include <cuda_gl_interop.h>
 #include <cuda.h>
 #include <cudaGL.h>
 #endif  // HAVE_CUDA
 
-// #include <vector>
-// #include <memory>
-// #include <iostream>
-// #include <unordered_map>
-// #include <string>
-// #include <stdexcept>
-// #include <cstdint>
-// #include <unordered_map>
 #include <unordered_set>
 
 namespace QueryRenderer {
@@ -37,13 +20,9 @@ class QueryRenderManager;
 #ifdef HAVE_CUDA
 struct CudaHandle {
   void* handle;
-
-  // GLuint handle;
   unsigned int numBytes;
 
   CudaHandle(void* handle, unsigned int numBytes) : handle(handle), numBytes(numBytes) {}
-
-  // CudaHandle(GLuint handle, unsigned int numBytes) : handle(handle), numBytes(numBytes) {}
 };
 #endif
 
@@ -128,7 +107,8 @@ class QueryResultVertexBuffer : public QueryVertexBuffer {
   // static void checkCudaErrors(CUresult result);
   void checkCudaErrors(CUresult result);
 
-  CUgraphicsResource _getCudaGraphicsResource(bool registerResource = false);
+  void _initCudaGraphicsResource();
+  CUgraphicsResource _getCudaGraphicsResource();
 
   void _mapCudaGraphicsResource(CUgraphicsResource& rsrc);
 

commit 9801c9d9e7b4fea8661262a2d86d638dd745742e

render: fix segfault when startGpu > 0
cudaMgr expects deviceNum to be relative to startGpu, not relative to 0.
Passing the true deviceNum to cudaMgr->setContext will result in a segfault if
startGpu > 0.

Bug introduced in 2eab04db2033a0d6e89ade893bc5d923ffe16460.

CudaMgr/CudaMgr.cpp
@@ -118,6 +118,7 @@ void CudaMgr::printDeviceProperties() const {
 #endif
 }
 
+// deviceNum is the device number relative to startGpu (realDeviceNum - startGpu_)
 void CudaMgr::setContext(const int deviceNum) const {
 #ifdef HAVE_CUDA
   // assert (deviceNum < deviceCount_);
@@ -173,6 +174,7 @@ void CudaMgr::copyDeviceToHost(int8_t* hostPtr, const int8_t* devicePtr, const s
 #endif
 }
 
+// destDeviceNum and srcDeviceNum are the device numbers relative to startGpu (realDeviceNum - startGpu_)
 void CudaMgr::copyDeviceToDevice(int8_t* destPtr,
                                  int8_t* srcPtr,
                                  const size_t numBytes,

QueryRenderer/QueryRenderManager.cpp
@@ -139,7 +139,7 @@ void QueryRenderManager::_initialize(Rendering::WindowManager& windowMgr,
   for (size_t i = startDevice; i < endDevice; ++i) {
     if (cudaMgr) {
       // need to set a cuda context before creating gl/cuda interop buffers
-      cudaMgr->setContext(i);
+      cudaMgr->setContext(i - startDevice);
     }
 
     windowSettings.setStrSetting(StrSetting::NAME, windowName + std::to_string(i));