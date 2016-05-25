90b0c0b81182b9320d219e9025b6748368281b62
Alex Şuhan <alex@map-d.com>
2016-05-25 13:43:34 -0700

    Revert "[OF] Use single flag for compaction and overflow/underflow detection"
    This reverts commit 146ad5f408cb80d4eb9ad4591a2d1ccf5f8985b9.

--- QueryEngine/Execute.cpp
+++ QueryEngine/Execute.cpp
@@ -409,7 +409,7 @@ ResultRows Executor::execute(const Planner::RootPlan* root_plan,
         return ResultRows(rrows, queue_time_ms, render_time_ms);
       }
 #endif  // HAVE_RENDERING
-#ifdef ENABLE_COMPACTION
+#ifdef DETECT_OVERFLOW
       if (error_code == ERR_OVERFLOW_OR_UNDERFLOW) {
         throw std::runtime_error("Overflow or underflow");
       }
