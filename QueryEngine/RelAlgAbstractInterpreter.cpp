tomzhang/mapd-core

commit 0f77cbb5b449a4376be16f72232df47bc34ee5b7

Reject time / date / timestamp literals
Only support them through cast from literal string for now.

QueryEngine/RelAlgAbstractInterpreter.cpp
@@ -173,7 +173,7 @@ RexAbstractInput* parse_abstract_input(const rapidjson::Value& expr) noexcept {
   return new RexAbstractInput(json_i64(input));
 }
 
-RexLiteral* parse_literal(const rapidjson::Value& expr) noexcept {
+RexLiteral* parse_literal(const rapidjson::Value& expr) {
   CHECK(expr.IsObject());
   const auto& literal = field(expr, "literal");
   const auto type = to_sql_type(json_str(field(expr, "type")));
@@ -193,6 +193,12 @@ RexLiteral* parse_literal(const rapidjson::Value& expr) noexcept {
       return new RexLiteral(json_bool(literal), type, target_type, scale, precision, type_scale, type_precision);
     case kNULLT:
       return new RexLiteral(target_type);
+    case kTIME:
+    case kTIMESTAMP:
+    case kDATE: {
+      SQLTypeInfo lit_ti(type, false);
+      throw std::runtime_error("Unsupported literal type " + lit_ti.get_type_name());
+    }
     default:
       CHECK(false);
   }

QueryEngine/CalciteAdapter.cpp
@@ -408,6 +408,11 @@ class CalciteAdapter {
         const auto& target_ti = std::get<2>(parsed_lit);
         return makeExpr<Analyzer::Constant>(target_ti.get_type(), true, Datum{0});
       }
+      case kTIME:
+      case kTIMESTAMP:
+      case kDATE: {
+        throw std::runtime_error("Unsupported literal type " + lit_ti.get_type_name());
+      }
       default: { LOG(FATAL) << "Unexpected literal type " << lit_ti.get_type_name(); }
     }
     return nullptr;

Tests/ExecuteTest.cpp
@@ -1075,6 +1075,8 @@ TEST(Select, Time) {
     ASSERT_EQ(
         32, v<int64_t>(run_simple_agg("SELECT DATEPART(s, CAST('2007-10-30 12:15:32' AS TIMESTAMP)) FROM test;", dt)));
 #endif  // HAVE_CALCITE
+    EXPECT_THROW(run_simple_agg("SELECT DATEPART(s, TIMESTAMP '2007-10-30 12:15:32') FROM test;", dt),
+                 std::runtime_error);
     const auto rows = run_multiple_agg(
         "SELECT DATE_TRUNC(month, CAST(o AS TIMESTAMP(0))) AS key0, str AS key1, COUNT(*) AS val FROM test GROUP BY "
         "key0, key1 ORDER BY val DESC, key1;",

commit fc97920d2e2380ab078733441d0e323bfe1323b8

Make RexOperator constructor take unique pointers to operators
Fixes the leak introduced in 0f77cbb5b449a4376be16f72232df47bc34ee5b7.
Since we reject unsupported queries by throwing exceptions, we'll have
to make everything in RelAlgAbstractInterpreter use smart pointers and
ownership transfers. Start with RexOperator, in this patch.

QueryEngine/RelAlgAbstractInterpreter.cpp
@@ -228,10 +228,10 @@ RexOperator* parse_operator(const rapidjson::Value& expr) {
   const auto op = is_quantifier ? kFUNCTION : to_sql_op(op_name);
   const auto& operators_json_arr = field(expr, "operands");
   CHECK(operators_json_arr.IsArray());
-  std::vector<const RexScalar*> operands;
+  std::vector<std::unique_ptr<const RexScalar>> operands;
   for (auto operators_json_arr_it = operators_json_arr.Begin(); operators_json_arr_it != operators_json_arr.End();
        ++operators_json_arr_it) {
-    operands.push_back(parse_scalar_expr(*operators_json_arr_it));
+    operands.emplace_back(parse_scalar_expr(*operators_json_arr_it));
   }
   const auto type_it = expr.FindMember("type");
   CHECK(type_it != expr.MemberEnd());
@@ -325,9 +325,9 @@ JoinType to_join_type(const std::string& join_type_name) {
 const RexScalar* disambiguate_rex(const RexScalar*, const RANodeOutput&);
 
 const RexOperator* disambiguate_operator(const RexOperator* rex_operator, const RANodeOutput& ra_output) {
-  std::vector<const RexScalar*> disambiguated_operands;
+  std::vector<std::unique_ptr<const RexScalar>> disambiguated_operands;
   for (size_t i = 0; i < rex_operator->size(); ++i) {
-    disambiguated_operands.push_back(disambiguate_rex(rex_operator->getOperand(i), ra_output));
+    disambiguated_operands.emplace_back(disambiguate_rex(rex_operator->getOperand(i), ra_output));
   }
   return rex_operator->getDisambiguated(disambiguated_operands);
 }

QueryEngine/RelAlgAbstractInterpreter.h
@@ -145,14 +145,10 @@ class RexLiteral : public RexScalar {
 
 class RexOperator : public RexScalar {
  public:
-  RexOperator(const SQLOps op, const std::vector<const RexScalar*> operands, const SQLTypeInfo& type)
-      : op_(op), type_(type) {
-    for (auto operand : operands) {
-      operands_.emplace_back(operand);
-    }
-  }
+  RexOperator(const SQLOps op, std::vector<std::unique_ptr<const RexScalar>>& operands, const SQLTypeInfo& type)
+      : op_(op), operands_(std::move(operands)), type_(type) {}
 
-  virtual const RexOperator* getDisambiguated(const std::vector<const RexScalar*>& operands) const {
+  virtual const RexOperator* getDisambiguated(std::vector<std::unique_ptr<const RexScalar>>& operands) const {
     return new RexOperator(op_, operands, type_);
   }
 
@@ -249,10 +245,12 @@ class RexCase : public RexScalar {
 
 class RexFunctionOperator : public RexOperator {
  public:
-  RexFunctionOperator(const std::string& name, const std::vector<const RexScalar*> operands, const SQLTypeInfo& ti)
+  RexFunctionOperator(const std::string& name,
+                      std::vector<std::unique_ptr<const RexScalar>>& operands,
+                      const SQLTypeInfo& ti)
       : RexOperator(kFUNCTION, operands, ti), name_(name) {}
 
-  virtual const RexOperator* getDisambiguated(const std::vector<const RexScalar*>& operands) const override {
+  virtual const RexOperator* getDisambiguated(std::vector<std::unique_ptr<const RexScalar>>& operands) const {
     return new RexFunctionOperator(name_, operands, getType());
   }
 