18bdb7dad8b6a6cf764037195f8d0d520a7b8b70
Alex Şuhan <alex.suhan@gmail.com>
2017-04-20 19:33:52 -0700

    Fix multi-node performance for IN subquery with big result
    We have to save the round-trips, the right hand side can be ~30M
    entries. Going that many times over network isn't feasible.

--- StringDictionary/string_dictionary.thrift
+++ StringDictionary/string_dictionary.thrift
@@ -6,5 +6,6 @@ service RemoteStringDictionary {
   list<string> get_like(1: string pattern, 2: bool icase, 3: bool is_simple, 4: string escape, 5: i64 generation, 6: i32 dict_id)
   list<string> get_regexp_like(1: string pattern, 2: string escape, 3: i64 generation, 4: i32 dict_id)
   list<i32> get_or_add_bulk(1: list<string> strings, 2: i32 dict_id)
+  list<i32> translate_string_ids(1: i32 dest_dict_id, 2: list<i32> source_ids, 3: i32 source_dict_id, 4: i32 dest_generation)
   bool checkpoint(1: i32 dict_id)
 }
